a013a479db4b992dd344cbbfcf6acbb342befb16
IDStack <idstack@outlook.co.id>
2020-03-19 23:54:27 +0700

    create post resource controller

---
+++ controllers/post.controller.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+const Post = db.posts;
+const Op = db.Sequelize.Op;
+
+// Create and Save a new Post
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all Posts from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single Post with an id
+exports.findOne = (req, res) => {
+
+};
+
+// Update a Post by the id in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a Post with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+
+// Delete all Posts from the database.
+exports.deleteAll = (req, res) {
+
+};
+
+// Find all published Posts
+exports.findAllPublished = (req, res) => {
+
+};
