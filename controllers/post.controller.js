3b1a7006b26145bcd86a8c0090bad3e017d83457
IDStack <idstack@outlook.co.id>
2020-03-20 00:03:16 +0700

    create a new object post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -4,7 +4,31 @@ const Op = db.Sequelize.Op;
 
 // Create and Save a new Post
 exports.create = (req, res) => {
+    // Validate request
+    if (!req.body.title) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return ;
+    }
 
+    // Create a Post
+    const post = {
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    };
+
+    // Save Post in the database
+    Post.create(post)
+        .then((data) => {
+            res.send(data);
+        }).catch((err) => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Post."
+            })
+        });
 };
 
 // Retrieve all Posts from the database.
@@ -28,7 +52,7 @@ exports.delete = (req, res) => {
 };
 
 // Delete all Posts from the database.
-exports.deleteAll = (req, res) {
+exports.deleteAll = (req, res) => {
 
 };
 
