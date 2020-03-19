29857a9f6cd5733571899fb5c59a0afe9e28c70a
IDStack <idstack@outlook.co.id>
2020-03-20 00:44:52 +0700

    retrieve a single object post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -49,7 +49,16 @@ exports.findAll = (req, res) => {
 
 // Find a single Post with an id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Post.findByPk(id)
+        .then((data) => {
+            res.send(data);
+        }).catch((err) => {
+            res.status(500).send({
+                message: "Error retrieving Tutorial with id=" + id
+            });
+        });
 };
 
 // Update a Post by the id in the request
