8ccfda620c0ba4a57223209f73ff07355ec45a6b
IDStack <idstack@outlook.co.id>
2020-03-19 23:12:08 +0700

    initialize sequelize

--- server.js
+++ server.js
@@ -16,6 +16,17 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/**
+ * In development, you may need to drop existing tables and re-sync database. 
+ * Just use force: true as following code:
+ * 
+ * db.sequelize.sync({ force: true }).then(() => {
+ *  console.log("Drop and re-sync db.");
+ * });
+ */
+const db = require("./models");
+db.sequelize.sync();
+
 // Simple Route
 app.get("/", (req, res) => {
     res.json({
@@ -27,5 +38,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}/`);
 });
