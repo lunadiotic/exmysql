15f90c55b3a231613ea727e33f93922f02a68d1e
IDStack <idstack@outlook.co.id>
2020-03-20 02:42:06 +0700

    update a single object post

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -63,7 +63,25 @@ exports.findOne = (req, res) => {
 
 // Update a Post by the id in the request
 exports.update = (req, res) => {
+    const id = req.params.id;
 
+    Post.update(req.body, {
+        where: {id: id}
+    }).then((result) => {
+        if (result == 1) {
+            res.send({
+                message: "Tutorial was updated successfully."
+            })
+        } else {
+            res.send({
+                message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+            });
+        }
+    }).catch((err) => {
+        res.status(500).send({
+            message: "Error updating Tutorial with id=" + id
+        });
+    });
 };
 
 // Delete a Post with the specified id in the request
